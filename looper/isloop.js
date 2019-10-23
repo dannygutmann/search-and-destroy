6c31e6cac876a9977987bb29566c95737f576730
Danny Gutmann <dannycgutmann@gmail.com>
2019-10-23 15:51:01 -0400

    Completed isLoop exercise

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,21 @@
-'use strict';
+"use strict";
 
 //Complete this algo
-const isLoop = (linkedlist) => {
+const isLoop = linkedlist => {
+  let arr = [];
+  let currentNode = linkedlist.head;
 
+  while (currentNode.value !== linkedlist.tail.value) {
+    if (arr.includes(currentNode.value)) {
+      return true;
+    }
+
+    arr.push(currentNode.value);
+    currentNode = currentNode.next;
+  }
+
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +25,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
