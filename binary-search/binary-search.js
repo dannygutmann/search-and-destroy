83a302faeda64abfabe6cf9f37fb38336913e555
Gerard Shapiro <gshapiro78@gmail.com>
2019-10-23 15:14:05 -0400

    fixed bindary

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,23 @@
 'use strict';
 
 // Complete this algo
+
+
 const binarySearch = (array, target) => {
-	
-};
+	let mid=Math.floor(array.length/2);
+	if(target===array[mid]){
+		return true
+	}
+	else if(target<array[mid]&&array.length>1){
+		return binarySearch(array.slice(0,mid),target)
+	}
+	else if(target>array[mid]&& array.length>1){
+		return binarySearch(array.slice(mid),target)
+	}
+	else {
+		return false;
+	}
+}
 
 /*
 	EXTRA CREDIT:
